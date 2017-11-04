34e3c7acbb5e17a6cd36194ff08b1fcfe5e1810f
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-04 15:25:13 -0400

    add carousel arrow

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+main-carousel [ "img/computers2.jpeg", "img/computerwrench.jpg"]
+
+
+var carousel = document.getElementsById("main-image")
+var button = document.getElmentById("arrow")
+
+
+function swapPic() {
+  let x = 0;
+  carousel.src = main-carousel[x++]
+}
+
+
+button.addEventListener('click', swapPic)
