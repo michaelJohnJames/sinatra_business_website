83ae62722d8fcec8094837bb0bd9bd02aa0874d9
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-11-06 17:53:31 -0500

    add filter to nav bar

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ end
 
 post '/' do
   from = Email.new(email: params['email'])
-  to = Email.new(email: 'mjj5002@gmail.com')
+  to = Email.new(email: "mjj5002@gmail.com")
   subject = params['email-subject']
   content = Content.new(type: 'text/plain', value: params['email-message'])
   mail = Mail.new(from, subject, to, content)
